bb28b9dc2372783cb7a67b5ac766c11c72511697
Vahid Spacer <108230053+Vahid-Spacer@users.noreply.github.com>
2023-12-12 18:41:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ while true; do
     echo "   5) exit"
     echo -e "${YELLOW}+--------------------------------------------------+${NC}"
     echo -e ""
-    read -p "Please select one [1-2-3-4-5]: " choice
+    read -p "Please select one [1-2-3-4-5]: " ${GREEN}choice${NC}
 
 case $choice in
 1)
